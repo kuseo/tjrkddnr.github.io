596260ce0b4ef6478bad697d3c0be52a9d504369
서강욱 <tjrkddnr@korea.ac.kr>
2018-12-01 01:51:15 +0900

    resolve scrolling offset problem

--- js/MyTree.js
+++ js/MyTree.js
@@ -112,18 +112,10 @@ function init(tree){
 }
 
 function getPosition(event, tree){
-    var x = new Number();
-    var y = new Number();
-    var canvas = document.getElementById("canvas");
-
-    if (event.x != undefined && event.y != undefined) {
-        x = event.x;
-        y = event.y;
-    } else {
-        console.log('Error');
-    }
-    x -= canvas.offsetLeft;
-    y -= canvas.offsetTop;
+    var canvas = document.getElementById('canvas');
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
 
     tree.printClickeObject(x, y);
 }
